c91f8c5994b801a1bbfafbecb1163ec787bb7e40
win htet naing <37439378+winhtetnaing@users.noreply.github.com>
2018-06-01 12:23:46 +0630

    ad
    ad

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var mongo = require("mongoose");
 var db = 
 /*mongo.connect("mongodb://127.0.0.1:27017/reactcrud", function(err, response){*/
-mongo.connect("mongodb://xpote:74GNRFLGOnEhSqH3@cluster0-shard-00-00-xcgtt.mongodb.net:27017,cluster0-shard-00-01-xcgtt.mongodb.net:27017,cluster0-shard-00-02-xcgtt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", function(err, response){
+mongo.connect("mongodb://xpote:74GNRFLGOnEhSqH3@cluster0-shard-00-00-xcgtt.mongodb.net:27017,cluster0-shard-00-01-xcgtt.mongodb.net:27017,cluster0-shard-00-02-xcgtt.mongodb.net:27017/injection?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", function(err, response){
    if(err){ console.log('Failed to connect to ' + db); }
    else{ console.log('Connected  ');}// + db, ' + ', response); }
 });
